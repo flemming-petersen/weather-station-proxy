8631105cb0a80097250800f4b2a89da0db1b83f7
Flemming Petersen <mail@flemming-petersen.de>
2023-06-21 19:25:25 +0000

    chore: add details to edit view

--- resources/views/templates/admin/edit.blade.php
+++ resources/views/templates/admin/edit.blade.php
@@ -32,37 +32,43 @@
             <x-form.label name="description">Beschreibung</x-form.label>
             <x-form.text name="description" value="{{ $station->description }}" />
         </div>
-        <div class="flex flex-col">
-            <x-form.label name="windy_station_id">Windy Station Id Key</x-form.label>
-            <x-form.text name="windy_station_id" value="{{ $station->windy_station_id }}" />
-        </div>
-        <div class="flex flex-col">
-            <x-form.label name="windy_key">Windy Api Key</x-form.label>
-            <x-form.text name="windy_key" value="{{ $station->windy_key }}" />
-        </div>
-        <div class="flex flex-col">
-            <x-form.label name="windy_url">Windy Stations Url</x-form.label>
-            <x-form.text name="windy_url" value="{{ $station->windy_url }}" />
-        </div>
-        <div class="flex flex-col">
-            <x-form.label name="windguru_uid">Windguru Uid</x-form.label>
-            <x-form.text name="windguru_uid" value="{{ $station->windguru_uid }}" />
-        </div>
-        <div class="flex flex-col">
-            <x-form.label name="windguru_salt">Windguru Salt</x-form.label>
-            <x-form.text name="windguru_salt" value="{{ $station->windguru_salt }}" />
-        </div>
-        <div class="flex flex-col">
-            <x-form.label name="windguru_password">Windguru Password</x-form.label>
-            <x-form.text name="windguru_password" value="{{ $station->windguru_password }}" />
-        </div>
-        <div class="flex flex-col">
-            <x-form.label name="windguru_url">Windguru Url</x-form.label>
-            <x-form.text name="windguru_url" value="{{ $station->windguru_url }}" />
-        <div class="flex flex-col">
-            <x-form.label name="windguru_widget">Windguru Widget</x-form.label>
-            <x-form.textarea name="windguru_widget" value="{{ $station->windguru_widget }}" />
-        </div>
+        <details class="mb-4">
+            <summary>Windy</summary>
+            <div class="flex flex-col">
+                <x-form.label name="windy_station_id">Windy Station Id Key</x-form.label>
+                <x-form.text name="windy_station_id" value="{{ $station->windy_station_id }}" />
+            </div>
+            <div class="flex flex-col">
+                <x-form.label name="windy_key">Windy Api Key</x-form.label>
+                <x-form.text name="windy_key" value="{{ $station->windy_key }}" />
+            </div>
+            <div class="flex flex-col">
+                <x-form.label name="windy_url">Windy Stations Url</x-form.label>
+                <x-form.text name="windy_url" value="{{ $station->windy_url }}" />
+            </div>
+        </details>
+        <details class="mb-4">
+            <summary>Windguru</summary>
+            <div class="flex flex-col">
+                <x-form.label name="windguru_uid">Windguru Uid</x-form.label>
+                <x-form.text name="windguru_uid" value="{{ $station->windguru_uid }}" />
+            </div>
+            <div class="flex flex-col">
+                <x-form.label name="windguru_salt">Windguru Salt</x-form.label>
+                <x-form.text name="windguru_salt" value="{{ $station->windguru_salt }}" />
+            </div>
+            <div class="flex flex-col">
+                <x-form.label name="windguru_password">Windguru Password</x-form.label>
+                <x-form.text name="windguru_password" value="{{ $station->windguru_password }}" />
+            </div>
+            <div class="flex flex-col">
+                <x-form.label name="windguru_url">Windguru Stations Url</x-form.label>
+                <x-form.text name="windguru_url" value="{{ $station->windguru_url }}" />
+            <div class="flex flex-col">
+                <x-form.label name="windguru_widget">Windguru Widget</x-form.label>
+                <x-form.textarea name="windguru_widget" value="{{ $station->windguru_widget }}" />
+            </div>
+        </details>
         <button class="rounded bg-cyan-400 p-4" type="submit">Speichern</button>
     </form>
 </x-card>
